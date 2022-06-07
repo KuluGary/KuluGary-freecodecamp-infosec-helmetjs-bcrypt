c8952c5cf4788841e7955b34bbdd3476cad98e07
Gary <kululu.gary@gmail.com>
2022-06-07 11:35:13 +0200

    bcrypt tasks

--- server.js
+++ server.js
@@ -1,53 +1,30 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const bcrypt = require("bcrypt");
 
-
+bcrypt.hash("passw0rd!", 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare("passw0rd!", hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
